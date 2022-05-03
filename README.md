12c93925c8de897209690f3aee3038011ed5d681
Ugo <nozolo96@gmail.com>
2022-05-03 12:41:04 +0100

    Updated readme. Added images

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # Remote-Sensor-Node
-This project uses the mcci LMIC library to create a LoraWAN sensor node.
+An ultlra-low-power LoRaWAN sensor node based on the MCCI LMIC Library. This repository contiains the source code, mechanical design files, the electronic schematic and some images of the project.
 
+## Features
+- 14.5μA sleep current
+- 10 minute transmission interval
+- 6km range achieved to a public gateway from ground level
+
+## Planned for V2
+- Battery monitoring
+- Custom PCB
+- Ratiation shield for sensors
 
 ## Blog
 [Solar Powered Sensor](https://ugo-uzoukwu.blogspot.com/)
 
 ## Videos
-[Video Playlist](https://www.youtube.com/playlist?list=PLkDD2GJCGW-Zxzu5pHdPQPp9Yhqgw_unU)
+[YouTube Playlist](https://www.youtube.com/playlist?list=PLkDD2GJCGW-Zxzu5pHdPQPp9Yhqgw_unU)
+
+## Live Data
+[Tingspeak]()
--- src/main.cpp
+++ src/main.cpp
@@ -219,22 +219,15 @@ void sleep(osjob_t* j){
 
 void setupChannelsEU868(){    
    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-   LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
-   LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-   LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-   LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-   LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-   LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-   LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-   LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-// //    LMIC_disableChannel(1);
-// //    LMIC_disableChannel(2);
-// //    LMIC_disableChannel(3);
-// //    LMIC_disableChannel(4);
-// //    LMIC_disableChannel(5);
-// //    LMIC_disableChannel(6);
-// //    LMIC_disableChannel(7);
-// //    LMIC_disableChannel(8); 
+
+    LMIC_disableChannel(1);
+    LMIC_disableChannel(2);
+    LMIC_disableChannel(3);
+    LMIC_disableChannel(4);
+    LMIC_disableChannel(5);
+    LMIC_disableChannel(6);
+    LMIC_disableChannel(7);
+    LMIC_disableChannel(8); 
 }
 
 void onEvent (ev_t ev) {
